4a0affa6626d7425616e0e1656678bfe48650e93
in-kim <ehddls960617@naver.com>
2023-02-06 23:23:18 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   experimental: {
     appDir: true,
+    fontLoaders: [{ loader: "@next/font/google" }],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,19 @@
-import './globals.css'
+import "./globals.css";
+import { Inter } from "@next/font/google";
+import Nav from "./analytics/Nav/page";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="ko">
       <head />
-      <body>{children}</body>
+      <body>
+        <Nav />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
