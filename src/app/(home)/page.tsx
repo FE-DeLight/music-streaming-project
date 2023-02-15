be6283b23e5c81a6c9dab4d30b7198764eef9e08
in-kim <ehddls960617@naver.com>
2023-02-15 22:48:09 +0900

    jest test 환경 추가

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,9 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <div className={styles.wrap}>home</div>;
+  return (
+    <div className={styles.wrap}>
+      <h1>home</h1>
+    </div>
+  );
 }
