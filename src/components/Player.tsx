1b049760e3a9cfa1968e2a1b975a0603497adb6b
yujin <yujin@dfluid.kr>
2023-03-07 11:34:56 +0900

    Add : 플레이어 레이아웃 구현 플레이어바, 플레이리스트 레이아웃 구현

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -5,31 +5,66 @@ import { routes } from "./route";
 
 export default function Nav(): JSX.Element {
   const router = useRouter();
+  const [like, setLike] = useState(false);
   const [soundRange, setSoundRange] = useState(30);
+  const [player, setPlayer] = useState(false);
+  const [tab, setTab] = useState(0);
+  const [musicList, setMusicList] = useState(true);
+
+  const tabMenu = [{ name: "음악" }, { name: "가사" }];
+
+  const clickLike = () => {
+    setLike((prev) => (prev = !prev));
+  };
+  const openPlaylist = () => {
+    setPlayer((prev) => (prev = !prev));
+  };
+  const clickTab = () => {};
+  const clickMusicList = () => {
+    setMusicList((prev) => (prev = !prev));
+  };
 
   return (
     <div className="player">
-      <div className="list">
+      {/* 플레이어 리스트 */}
+      <div className={`list ${player && "list--active"}`}>
         <div className="list__left-area">
-          <strong className="list__left-title">제목</strong>
-          <div className="list__left-singer">가수</div>
-          <div className="list__left-thumb">{/* <img src="" alt="" /> */}</div>
-          <div className="list__left-btn-area">
-            <button className="store-btn">담기</button>
-            <button className="show-more-btn">더보기</button>
+          <div className="list__left-area-inner">
+            <Link href="/" style={{ textDecoration: "none", display: "block" }}>
+              <span className="list__left-title">제목</span>
+            </Link>
+            <Link href="/" style={{ textDecoration: "none", display: "block" }}>
+              <span className="list__left-singer">가수</span>
+            </Link>
+            <div className="list__left-thumb">
+              {/* <img src="" alt="" /> */}
+            </div>
+            <div className="list__left-btn-area">
+              <button className="store-btn">
+                <span className="blind">담기</span>
+              </button>
+              <button className="show-more-btn">
+                <span className="blind">더보기</span>
+              </button>
+            </div>
           </div>
         </div>
 
         <div className="list__right-area">
           <div className="list__right-btn-area">
-            <button className="setting-btn">설정</button>
-            <button className="close-btn">닫기</button>
+            <button className="setting-btn">
+              <span className="blind">설정</span>
+            </button>
+            <button className="close-btn" onClick={openPlaylist}>
+              <span className="blind">닫기</span>
+            </button>
           </div>
           <div className="tab">
             <div className="tab-head">
               <div className="tab-head__title-area">
-                <div className="tab-head__title">음악</div>
-                <div className="tab-head__title">가사</div>
+                {tabMenu.map((tab, index) => (
+                  <button key={index}>{tab.name}</button>
+                ))}
               </div>
               <button className="edit-btn">편집</button>
             </div>
@@ -49,36 +84,52 @@ export default function Nav(): JSX.Element {
                 </div>
               </div>
               <div className="tab-body__list-area">
-                <div className="music-list">
-                  <div className="music-list__head">
-                    <div className="music-list__head-left">
-                      <div className="music-list__title">제목 or 가수</div>
+                <div
+                  className={`music-list ${!musicList && "music-list--fold"}`}
+                >
+                  <div className="music-list_top">
+                    <div className="music-list_top-left">
+                      <div className="music-list__list-title">
+                        플레이리스트 이름
+                      </div>
                     </div>
-                    <div className="music-list__head-right">
-                      <button className="music-list__play-btn">재생</button>
-                      <button className="music-list__list-fold-btn">
-                        접기
+                    <div className="music-list_top-right">
+                      <button className="music-list__play-btn">
+                        <span className="blind">재생</span>
+                      </button>
+                      <button
+                        className="music-list__list-fold-btn"
+                        onClick={clickMusicList}
+                      >
+                        <span className="blind">접기</span>
                       </button>
                     </div>
                   </div>
-                  <div className="music-list__body">
+                  <div className="music-list__content">
                     <div className="music-list__music">
-                      <div className="music-list__thumb">
-                        {/* <img src="" alt="" /> */}
-                      </div>
-                      <div className="music-list__info">
-                        <div className="music-list__title">제목</div>
-                        <div className="music-list__singer">가수</div>
-                      </div>
-                      <div className="music-list__show-more-btn">더보기</div>
+                      <button className="music-list__music-btn">
+                        <div className="music-list__thumb">
+                          {/* <img src="" alt="" /> */}
+                        </div>
+                        <div className="music-list__info">
+                          <div className="music-list__title">제목</div>
+                          <div className="music-list__singer">가수</div>
+                        </div>
+                      </button>
+                      <button className="music-list__show-more-btn">
+                        <span className="blind">더보기</span>
+                      </button>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
+            <div className="tab-body tab-body--lyrics">노래가사</div>
           </div>
         </div>
       </div>
+
+      {/* 플레이어 바 */}
       <div className="bar">
         <div className="progress">
           <div className="progress-bar" />
@@ -93,15 +144,30 @@ export default function Nav(): JSX.Element {
               <div className="title">제목</div>
               <div className="singer">가수</div>
             </div>
-            <button className="like-btn">좋아요</button>
+            <button
+              className={like ? "like-btn--active" : "like-btn"}
+              onClick={clickLike}
+            >
+              <span className="blind">좋아요</span>
+            </button>
           </div>
 
           <div className="bar__center-area">
-            <button className="repeat-btn">반복재생</button>
-            <button className="prev-btn">이전곡</button>
-            <button className="play-btn">재생</button>
-            <button className="next-btn">다음곡</button>
-            <button className="random-btn">랜덤재생</button>
+            <button className="repeat-btn">
+              <span className="blind">반복재생</span>
+            </button>
+            <button className="prev-btn">
+              <span className="blind">이전곡</span>
+            </button>
+            <button className="play-btn">
+              <span className="blind">재생</span>
+            </button>
+            <button className="next-btn">
+              <span className="blind">다음곡</span>
+            </button>
+            <button className="random-btn">
+              <span className="blind">랜덤재생</span>
+            </button>
             <div className="time">
               <span className="current-time">00:10</span>
               <span> / </span>
@@ -110,25 +176,295 @@ export default function Nav(): JSX.Element {
           </div>
 
           <div className="bar__right-area">
-            <button className="mute-btn">음소거</button>
+            <button className="mute-btn">
+              <span className="blind">음소거</span>
+            </button>
             <input
               className="sound-range-input"
               type="range"
               value={soundRange}
             />
-            <button className="playlist-btn">재생목록</button>
+            <button className="playlist-btn" onClick={openPlaylist}>
+              <span className="blind">재생목록</span>
+            </button>
           </div>
         </div>
       </div>
 
       <style jsx>{`
+         {
+          /* 공통 */
+        }
         * {
           box-sizing: border-box;
         }
         button {
-          color: #ddd;
+          padding: 0;
+          border: 0;
+          color: #989898;
           background: transparent;
           cursor: pointer;
+        }
+        a {
+          text-decoration: none;
+        }
+        .blind {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 1px;
+          height: 1px;
+          clip: rect(0 0 0 0);
+          overflow: hidden;
+        }
+
+         {
+          /* 플레이어 리스트 */
+        }
+        .list {
+          position: fixed;
+          bottom: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          display: flex;
+          color: #989898;
+          background: rgba(0, 0, 0, 0.9);
+          transform: translateY(100%);
+          transition: all 0.5s;
+        }
+        .list--active {
+          transform: translateY(0);
+          transition: transform 0.5s;
+        }
+        .list__left-area {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex: 1 1 0;
+        }
+        .list__left-title {
+          display: block;
+          margin-bottom: 8px;
+          text-align: center;
+          font-size: 22px;
+          font-weight: bold;
+          color: #fff;
+        }
+        .list__left-singer {
+          display: block;
+          margin-bottom: 20px;
+          text-align: center;
+          font-size: 13px;
+          color: #989898;
+        }
+        .list__left-thumb {
+          width: 360px;
+          height: 360px;
+          border-radius: 10px;
+          background: #ddd;
+        }
+        .list__left-btn-area {
+          display: flex;
+          justify-content: center;
+          margin-top: 15px;
+        }
+        .store-btn,
+        .show-more-btn,
+        .setting-btn,
+        .close-btn {
+          width: 40px;
+          height: 40px;
+          background-size: contain;
+          background-position: center;
+          background-repeat: no-repeat;
+        }
+        .store-btn {
+          background-image: url("/icon_store.svg");
+        }
+        .show-more-btn {
+          background-image: url("/icon_show_more.svg");
+        }
+        .setting-btn {
+          background-image: url("/icon_setting.svg");
+        }
+        .close-btn {
+          background-image: url("/icon_close.svg");
+        }
+
+        .list__right-area {
+          width: 650px;
+          padding: 30px 40px;
+        }
+        .list__right-btn-area {
+          display: flex;
+          justify-content: flex-end;
+          gap: 15px;
+          margin-bottom: 20px;
+        }
+        .tab-head {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          border-bottom: 1px solid #444;
+        }
+        .tab-head__title-area {
+          display: flex;
+        }
+        .tab-head__title {
+          margin-right: 10px;
+          padding: 10px 0;
+          font-size: 16px;
+          color: hsla(0, 0%, 100%, 0.3);
+        }
+        .tab-head__title--active {
+          color: #fff;
+          border-bottom: 3px solid #7286ff;
+        }
+        .edit-btn {
+          font-size: 13px;
+          color: #aaa;
+        }
+        .tab-body__top {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          padding: 10px 0 20px;
+        }
+        .tab__search {
+          display: flex;
+          align-items: center;
+          width: 240px;
+          padding: 4px 10px;
+          border-radius: 30px;
+          background: hsla(0, 0%, 100%, 0.05);
+        }
+        .tab__search input {
+          width: 100%;
+          border: 0;
+          outline: 0;
+          font-size: 11px;
+          line-height: 1;
+          color: #bbb;
+          background: transparent;
+        }
+        .tab__search::before,
+        .list-btn::before {
+          content: "";
+          display: inline-block;
+          width: 24px;
+          height: 24px;
+        }
+        .tab__search::before {
+          background: url("/icon_search.svg") no-repeat center / contain;
+        }
+        .list-btn {
+          display: flex;
+          align-items: center;
+        }
+        .list-btn::before {
+          background: url("/icon_store.svg") no-repeat center / contain;
+        }
+        .tab-body__top-right {
+          display: flex;
+          align-items: center;
+          gap: 15px;
+        }
+        .music-list {
+          border-radius: 5px;
+          background: hsla(0, 0%, 100%, 0.1);
+        }
+        .music-list--fold .music-list__list-fold-btn {
+          transform: rotate(0);
+        }
+        .music-list--fold .music-list__content {
+          display: none;
+        }
+        .music-list_top {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          padding: 10px 20px 10px 15px;
+        }
+        .music-list__list-title {
+          font-size: 14px;
+          font-weight: bold;
+          color: #fff;
+        }
+        .music-list_top-right {
+          display: flex;
+          gap: 15px;
+        }
+        .music-list__play-btn,
+        .music-list__list-fold-btn,
+        .music-list__show-more-btn {
+          width: 30px;
+          height: 30px;
+          background-size: contain;
+          background-position: center;
+          background-repeat: no-repeat;
+        }
+        .music-list__play-btn {
+          background-image: url("/icon_play_list.svg");
+        }
+        .music-list__list-fold-btn {
+          transform: rotate(180deg);
+          background-image: url("/icon_fold.svg");
+        }
+        .music-list__show-more-btn {
+          background-image: url("/icon_show_more.svg");
+        }
+        .music-list__content {
+          padding: 10px 20px 20px 15px;
+        }
+        .music-list__content--fold {
+          display: none;
+        }
+        .music-list__music {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        }
+        .music-list__music-btn {
+          display: flex;
+          align-items: center;
+        }
+        .music-list__thumb {
+          width: 45px;
+          height: 45px;
+          margin-right: 16px;
+          border-radius: 4px;
+          background: #ddd;
+        }
+        .music-list__thumb img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
+        .music-list__info {
+          width: 340px;
+          text-align: left;
+        }
+        .music-list__title {
+          margin-bottom: 3px;
+          font-size: 14px;
+          font-weight: bold;
+          color: #fff;
+        }
+        .music-list__singer {
+          font-size: 11px;
+        }
+
+        .tab-body--lyrics {
+          width: 100%;
+          padding: 10px 20px 10px 15px;
+          border-radius: 5px;
+          font-size: 14px;
+          background: hsla(0, 0%, 100%, 0.1);
+        }
+
+         {
+          /* 플레이어 바 */
         }
         .bar {
           position: fixed;
@@ -141,27 +477,33 @@ export default function Nav(): JSX.Element {
         }
         .progress {
           width: 100%;
-          height: 3px;
+          height: 2px;
+          background: #333;
         }
         .progress-bar {
           width: 30%;
-          height: 3px;
+          height: 2px;
           border-radius: 0 2px 2px 0;
           background: #576aff;
         }
         .controller {
           display: flex;
-          justify-content: space-between;
           align-items: center;
           width: 100%;
           height: 100%;
-          padding: 0 20px;
+          padding: 0 30px;
         }
         .bar__left-area,
         .bar__center-area,
         .bar__right-area {
           display: flex;
           align-items: center;
+        }
+        .bar__left-area {
+          width: 45%;
+        }
+        .bar__right-area {
+          margin-left: auto;
         }
         .thumb {
           width: 40px;
@@ -185,13 +527,75 @@ export default function Nav(): JSX.Element {
         .singer {
           font-size: 11px;
         }
-        .center-area {
-          gap: 5px;
+        .like-btn {
+          background-image: url("/icon_like_off.svg");
+        }
+        .like-btn--active {
+          background-image: url("/icon_like_on.svg");
+        }
+        .like-btn img {
+          width: 100%;
+          height: 100%;
+          object-fit: contain;
+        }
+        .bar__center-area,
+        .bar__right-area {
+          gap: 10px;
+        }
+        .like-btn,
+        .like-btn--active,
+        .repeat-btn,
+        .prev-btn,
+        .play-btn,
+        .next-btn,
+        .next-btn,
+        .random-btn,
+        .mute-btn,
+        .playlist-btn {
+          width: 44px;
+          height: 44px;
+          background-size: contain;
+          background-position: center;
+          background-repeat: no-repeat;
+        }
+        :is(
+            .repeat-btn,
+            .prev-btn,
+            .play-btn,
+            .next-btn,
+            .next-btn,
+            .random-btn
+          ):hover {
+          transform: scale(1.1);
+        }
+        .repeat-btn {
+          background-image: url("/icon_repeat_play.svg");
+        }
+        .prev-btn {
+          background-image: url("/icon_prev.svg");
+        }
+        .play-btn {
+          background-image: url("/icon_play.svg");
+        }
+        .next-btn {
+          background-image: url("/icon_next.svg");
+        }
+        .random-btn {
+          background-image: url("/icon_random_play.svg");
+        }
+        .mute-btn {
+          width: 35px;
+          height: 35px;
+          background-image: url("/icon_sound.svg");
+        }
+        .playlist-btn {
+          background-image: url("/icon_player.svg");
         }
         .time {
           display: flex;
           align-items: center;
           gap: 5px;
+          margin-left: 10px;
           font-size: 13px;
         }
         .current-time {
@@ -220,15 +624,6 @@ export default function Nav(): JSX.Element {
           width: 4px;
           height: 4px;
           background: red;
-        }
-
-        .list {
-          position: fixed;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-          background: rgba(0, 0, 0, 0.9);
         }
       `}</style>
     </div>
