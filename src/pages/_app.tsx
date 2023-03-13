cf51d3ae7365e5498f395d4f028af7666902d085
yujin <yujin@dfluid.kr>
2023-03-13 15:50:31 +0900

    Add : reset.css 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import Layout from "@/components/Layout";
 import { AppProps } from "next/app";
+import "../styles/reset.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
