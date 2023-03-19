95db73f0fe2076dfb3d5fb52eb512fb2ed709f72
in-kim <ehddls960617@naver.com>
2023-03-19 16:17:57 +0900

    Add : 카테고리별 리스트 api

---
+++ src/pages/api/categoryList.ts
@@ -0,0 +1,5 @@
+import { NextApiRequest, NextApiResponse} from "next";
+import MusicChart from './MusicChart.json';
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json(MusicChart);
+}
