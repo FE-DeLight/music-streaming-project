ac280b0d30c677d04e186bc47a1575bd455ff437
in-kim <ehddls960617@naver.com>
2023-03-20 23:09:17 +0900

    Update : lint 수정

--- src/components/Player/BlindText.tsx
+++ src/components/Player/BlindText.tsx
@@ -1,15 +1,15 @@
 import styled from 'styled-components';
 
 const BlindText = styled.span`
-   position: absolute;
-   top: 0;
-   left: 0;
-   width: 1px;
-   height: 1px;
-   clip: rect(0 0 0 0);
-   overflow: hidden;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 1px;
+  height: 1px;
+  clip: rect(0 0 0 0);
+  overflow: hidden;
 `;
 
 export default function Text(props: any): JSX.Element {
-   return <BlindText {...props}>{props.text}</BlindText>;
+  return <BlindText {...props}>{props.text}</BlindText>;
 }
--- src/components/Player/MusicListItem.tsx
+++ src/components/Player/MusicListItem.tsx
@@ -4,49 +4,49 @@ import PlayerButton from '@/components/Player/PlayerButton';
 import BlindText from '@/components/Player/BlindText';
 
 const MusicListItem = styled.li<{}>`
-   display: flex;
-   align-items: center;
-   justify-content: space-between;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-   & + & {
-      margin-top: 20px;
-   }
+  & + & {
+    margin-top: 20px;
+  }
 
-   .music-btn {
-      display: flex;
-      align-items: center;
-   }
+  .music-btn {
+    display: flex;
+    align-items: center;
+  }
 
-   .info {
-      width: 340px;
-      margin-left: 16px;
-      text-align: left;
-   }
+  .info {
+    width: 340px;
+    margin-left: 16px;
+    text-align: left;
+  }
 
-   .title {
-      margin-bottom: 6px;
-      font-size: 15px;
-      font-weight: 500;
-      color: #fff;
-   }
+  .title {
+    margin-bottom: 6px;
+    font-size: 15px;
+    font-weight: 500;
+    color: #fff;
+  }
 
-   .singer {
-      font-size: 12px;
-   }
+  .singer {
+    font-size: 12px;
+  }
 `;
 export default function Item(props: any): JSX.Element {
-   return (
-      <MusicListItem {...props}>
-         <button className="music-btn">
-            <PlayerThumb size={props.thumbSize} image={props.thumb} radius={props.thumbRadius} />
-            <div className="info">
-               <div className="title">{props.title}</div>
-               <div className="singer">{props.singer}</div>
-            </div>
-         </button>
-         <PlayerButton size={30} image={'/icon_show_more.svg'}>
-            <BlindText text={'더보기'} />
-         </PlayerButton>
-      </MusicListItem>
-   );
+  return (
+    <MusicListItem {...props}>
+      <button className="music-btn">
+        <PlayerThumb size={props.thumbSize} image={props.thumb} radius={props.thumbRadius} />
+        <div className="info">
+          <div className="title">{props.title}</div>
+          <div className="singer">{props.singer}</div>
+        </div>
+      </button>
+      <PlayerButton size={30} image={'/icon_show_more.svg'}>
+        <BlindText text={'더보기'} />
+      </PlayerButton>
+    </MusicListItem>
+  );
 }
