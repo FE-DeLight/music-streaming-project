3b41507d6c2e8d989ad42778f856f478f23ae82f
yujin <rosepark306@gmail.com>
2023-03-24 17:21:15 +0900

    Update : 클래스명 수정

--- src/components/Player/PlayList.tsx
+++ src/components/Player/PlayList.tsx
@@ -10,35 +10,35 @@ const PlayList = styled.div<{}>`
     background: hsla(0, 0%, 100%, 0.1);
   }
 
-  .play-list--fold .play-list__list-fold-btn {
+  .play-list--fold .list-fold-btn {
     transform: rotate(0);
   }
 
-  .play-list--fold .play-list__content {
+  .play-list--fold .content {
     display: none;
   }
 
-  .play-list_top {
+  .top {
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 10px 20px 10px 15px;
   }
 
-  .play-list__list-title {
+  .list-title {
     font-size: 14px;
     font-weight: bold;
     color: #fff;
   }
 
-  .play-list_top-right {
+  .top-right {
     display: flex;
     gap: 15px;
   }
 
-  .play-list__play-btn,
-  .play-list__list-fold-btn,
-  .play-list__show-more-btn {
+  .play-btn,
+  .list-fold-btn,
+  .show-more-btn {
     width: 30px;
     height: 30px;
     background-size: contain;
@@ -46,30 +46,30 @@ const PlayList = styled.div<{}>`
     background-repeat: no-repeat;
   }
 
-  .play-list__content {
+  .content {
     padding: 10px 20px 20px 15px;
   }
 
-  .play-list__content--fold {
+  .content--fold {
     display: none;
   }
 
-  .play-list__music {
+  .music {
     display: flex;
     align-items: center;
     justify-content: space-between;
   }
 
-  .play-list__music + .play-list__music {
+  .music + .music {
     margin-top: 20px;
   }
 
-  .play-list__play-btn {
+  .play-btn {
     display: flex;
     align-items: center;
   }
 
-  .play-list__thumb {
+  .thumb {
     width: 45px;
     height: 45px;
     margin-right: 16px;
@@ -77,37 +77,44 @@ const PlayList = styled.div<{}>`
     background: #ddd;
   }
 
-  .play-list__thumb img {
+  .thumb img {
     width: 100%;
     height: 100%;
     object-fit: cover;
   }
 
-  .play-list__info {
+  .info {
     width: 340px;
     text-align: left;
   }
 
-  .play-list__title {
+  .title {
     margin-bottom: 3px;
     font-size: 14px;
     font-weight: bold;
     color: #fff;
   }
 
-  .play-list__singer {
+  .singer {
     font-size: 11px;
   }
+
+  .none-search-data {
+    padding: 100px 0;
+    text-align: center;
+    font-size: 15px;
+  }
 `;
-export default function Button(props: any): JSX.Element {
+export default function List(props: any): JSX.Element {
+
   return (
     <PlayList {...props}>
       <div className={`play-list ${!props.isOpenPlayList && 'play-list--fold'}`}>
-        <div className="play-list_top">
-          <div className="play-list_top-left">
-            <div className="play-list__list-title">플레이리스트 이름</div>
+        <div className="top">
+          <div className="top-left">
+            <div className="list-title">플레이리스트 이름</div>
           </div>
-          <div className="play-list_top-right">
+          <div className="top-right">
             <PlayerButton size={30} image={'/icon_play_list.svg'}>
               <BlindText text={'재생'} />
             </PlayerButton>
@@ -121,19 +128,25 @@ export default function Button(props: any): JSX.Element {
             </PlayerButton>
           </div>
         </div>
-        <ul className="play-list__content">
-          {props.copyPlayerList.map((music: any, index: number) => {
-            return (
-              <MusicListItem
-                key={index}
-                thumb={music.album.imgList[0].url}
-                title={music.name}
-                singer={music.representationArtist.name}
-                thumbSize={45}
-                thumbRadius={4}
-              />
-            );
-          })}
+        <ul className="content">
+          {props.copyPlayerList.length > 0 ? (
+            props.copyPlayerList.map((music: any, index: number) => {
+              return (
+                <MusicListItem
+                  key={index}
+                  thumb={music.album.imgList[0].url}
+                  title={music.name}
+                  singer={music.representationArtist.name}
+                  thumbSize={45}
+                  thumbRadius={4}
+                  onClick={() => {props.onSelectMusic(index)}}
+                />
+              );
+            })
+          ) : (
+            <div className='none-search-data'>재생목록의 검색결과가 없습니다
+            </div>
+          )}
         </ul>
       </div>
     </PlayList>
