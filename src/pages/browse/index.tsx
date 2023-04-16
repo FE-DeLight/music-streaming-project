70bbc35ffeb7762f46d0a96767b4d79790d2b80d
Bae JaeKwan <toxictears84@gmail.com>
2023-04-16 14:49:06 +0900

    [Fix] browseList : 오타 수정

--- src/pages/browse/index.tsx
+++ src/pages/browse/index.tsx
@@ -40,8 +40,8 @@ export default function Browse() {
   const getData = async () => {
     // try, catch를 통해 에러 핸들링 해줘야 함.
     const res = await fetch('http://localhost:3000/api/categoryList', {
-      headers: {N
-        Accept: 'application/json',
+      headers: {
+        accept: 'application/json',
       },
     });
 
